ff49881a0d06bafcc326c0a023bd011c0355fd7c
somi37 <usman@marketlytics.com>
2017-03-02 13:14:01 +0500

    Update controllers.js

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,42 +1,67 @@
 'use strict';
 
-window.onload = function() {
-  var fileInput = document.getElementById('fileInput');
-  var fileDisplayArea = document.getElementById('fileDisplayArea');
-  var csvFile = {};
+segImport.controller('mainController', ['$scope', '$http',
+  function($scope, $http)
+{
+  var csv = $scope.csv = {};
 
-  fileInput.addEventListener('change', function(e) {
-    // Use DOM to get AngularJS root scope.
-    var scope = angular.element(this).scope();
+  csv.array = []; // These are the rows.
+  csv.JSON = []; // JSON object
+  csv.JSONString = '';
+  csv.writeKey = '';
 
-    // Reset firebase and local data store.
-    scope.$apply(function() {
-      scope.csv.removeAll();
+  // Take csv string and turn it into array.
+  csv.addArray = function addArray(csv) {
+    csv.forEach(function(row) {
+      this.array.push(row);
+    }.bind(this));
+    this.arrayToJSON();
+  };
+
+  // Empty rows in csv.array.
+  csv.removeAll = function removeAll() {
+    this.array.length = 0;
+  };
+
+  // Convert csv.array to csv.JSON.
+  csv.arrayToJSON = function arrayToJSON() {
+
+    var headers = this.array[0];
+    this.JSON.length = 0;
+
+    for (var i = 1; i < this.array.length; i ++) {
+      var obj = {};
+      var currentLine = this.array[i];
+      for (var j = 0; j < headers.length; j ++) {
+        if (headers[j].indexOf('.') > 0) {
+          var prefix = headers[j].substring(0, headers[j].indexOf('.'));
+          var suffix = headers[j].substring(headers[j].indexOf('.') + 1);
+          if (!obj[prefix])
+            obj[prefix] = {};
+          obj[prefix][suffix] = currentLine[j];
+        } else {
+          obj[headers[j]] = currentLine[j];
+        }
+      }
+      this.JSON.push(obj);
+    }
+
+    this.JSONString = JSON.stringify(this.JSON, null, 2);
+  };
+
+  // Post csv.JSON to end point.
+  csv.importJSON = function importJSON() {
+    console.log(this.JSON);
+    $http.post('/api/import', {batch: this.JSON, writeKey: this.writeKey})
+    .success(function(err, data) {
+      console.log(err);
+      console.log(data);
+      alert("data sent");
+    })
+    .error(function(err, data) {
+      console.log(err);
+      console.log(data);
+      alert("big ERROR ");
     });
-
-    var file = fileInput.files[0];
-    var textType = /text.*/;
-
-    if (file.type.match(textType)) {
-      var reader = new FileReader();
-
-      reader.onload = function(e) {
-        var csvString = reader.result;
-        // Clean the headers.
-        var firstLine = csvString.split('')[0];
-        var cleanedFirstLine = firstLine.replace(/\s+/g, '_');
-        csvString = csvString.replace(firstLine, cleanedFirstLine);
-
-        // Parse csv.
-        var csv = new CSV(csvString);
-        scope.$apply(function() {
-          scope.csv.addArray(csv);
-        });
-      };
-
-      reader.readAsText(file);
-    } else {
-      fileDisplayArea.innerText = "File not supported!"
-    };
-  });
-}
+  };
+}]);
